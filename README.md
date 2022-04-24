1c4860e173a68191d897c5c2423ca6148b50996a
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2022-04-24 20:39:39 +0300

    add readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+Instagram.com -> Save images from user saved tab(https://www.instagram.com/user_profile/saved/) to local machine in default browser saved folder.
+Remove images from instagram saved page of user after saved to local machine.
+Skip video but saved video links to localStorage.
--- instagram.js
+++ instagram.js
@@ -1,7 +1,3 @@
-// Instagram.com -> Save images from user saved tab(https://www.instagram.com/user_profile/saved/) to local machine in default browser saved folder.
-// Remove images from instagram saved page of user after saved to local machine.
-// Skip video but saved video links to localStorage.
-
 const selectorImg = ".FFVAD";
 const selectorElement = ".v1Nh3.kIKUG._bz0w";
 const selectorDialog = ".RnEpo._Yhr4";
